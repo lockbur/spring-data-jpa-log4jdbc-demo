c975c6406bb90bb7aeba6297117a2f9828e1085a
一名清官 <aboutdata@vip.qq.com>
2015-10-23 10:06:27 +0800

    test CI

--- src/main/java/com/spr/controller/ShopController.java
+++ src/main/java/com/spr/controller/ShopController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
